6498b0f7d2ea8bae7c1270f538ed43b0bd1ec63a
Nick-Menzhuber <nickmenzhuber@gmail.com>
2022-11-14 11:11:45 -0600

    building out routes

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -1,18 +1,28 @@
 const express = require('express');
+const pathfinderUI = require('pathfinder-ui')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use('/pathfinder', function(req, res, next){
+	pathfinderUI(app)
+	next()
+}, pathfinderUI.router)
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
 // Use apiRoutes
 app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+
+app.get("*", function(req, res){
+	res.redirect('/pathfinder');
+})
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
+
+// 
